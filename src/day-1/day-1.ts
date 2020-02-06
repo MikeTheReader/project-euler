993d7fbb968f5954446ccc909230553f0474007b
Michael Lundin <mikel@ifit.com>
2020-02-05 18:54:42 -0800

    feat: 1 - implement solution

--- src/day-1/day-1.ts
+++ src/day-1/day-1.ts
@@ -1,3 +1,7 @@
 export function multiplesOf3and5(highWaterMark:number): number {
-  return 0;
+  let sum = 0;
+  for (let i = 1; i < highWaterMark; i++) {
+    sum += (i % 3 === 0 || i % 5 === 0) ? i : 0;
+  }
+  return sum;
 }
